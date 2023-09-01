4f42c2c1d1361608188b9b1946fea4548252a823
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2023-09-01 16:22:14 -0300

    feature: menu buttons

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuButtons : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private string nameSceneToLoad;
+
+    public void GameNavigation()
     {
-        
+        SceneManager.LoadScene(nameSceneToLoad);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     // Update is called once per frame
