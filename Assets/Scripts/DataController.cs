b7f1e048811953c7fbbc8fcf4263d20b2e016abe
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2023-09-05 15:52:44 -0300

    handling game data

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class DataController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    [SerializeField] private Sprite volumeIcon;
+    [SerializeField] private TextAsset textJSON;
     void Start()
     {
-        
+        LoadGameData();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void LoadGameData()
+    {
+       JSONDATA = JsonUtility.FromJson<PlayerList>(textJSON.text);
+       System.Console.WriteLine(JSONDATA);
     }
 }
--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -12,14 +12,17 @@ public class MenuButtons : MonoBehaviour
     [SerializeField] private Sprite volumeIcon;
     [SerializeField] private Sprite muteIcon;
     [SerializeField] private Button volumeButton;
+    [SerializeField] private GameObject creditsPanel;
 
     void Start()
     {
+        //defines sprite for volume button when the menu appears
         volumeButton.GetComponent<Image>().sprite = volumeIcon;
     }
     
     public void ChangeButtonSprite()
     {
+        //changes the sprite of the volume button when clicked
         if (volumeButton.GetComponent<Image>().sprite == volumeIcon)
         {
             volumeButton.GetComponent<Image>().sprite = muteIcon;
@@ -27,6 +30,14 @@ public class MenuButtons : MonoBehaviour
         else
         {
             volumeButton.GetComponent<Image>().sprite = volumeIcon;
+        }
+    }
+
+    public void CreditsVisibility(){
+        if(creditsPanel.activeSelf){
+            creditsPanel.SetActive(false);
+        }else{
+            creditsPanel.SetActive(true);
         }
     }
 
