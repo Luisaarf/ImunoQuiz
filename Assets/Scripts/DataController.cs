2ba59d13b1e2f1a4af08b1c31e1094f3c341c7c6
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2023-09-21 18:46:58 -0300

    dataController

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class DataController : MonoBehaviour
 {
     [SerializeField] private TextAsset textJSON;
+
+    public int randNum;
     TextChange textChange;
+    
+    [SerializeField] generateIndex generateIndex;
+    string[] thisQuestionData = new string[6];
 
     [System.Serializable]
     public class Easy 
@@ -74,11 +79,106 @@ public class DataController : MonoBehaviour
         return theGameData.virus.easy[0].Question;
     }
 
+    //planos
+    public string[] sortQuestion(string thechoice, string choice){ 
+        theGameData = JsonUtility.FromJson<GameData>(textJSON.text);
+        randNum = generateIndex.generateIndexNumbers();
+        Debug.Log(randNum + " randNum");
+        if(thechoice == "virus"){
+            if(choice == "easy"){
+                thisQuestionData[0] = theGameData.virus.easy[randNum].Question;
+                thisQuestionData[1] = theGameData.virus.easy[randNum].CorrectAnswer;
+                thisQuestionData[2] = theGameData.virus.easy[randNum].AllAnswer[0];
+                thisQuestionData[3] = theGameData.virus.easy[randNum].AllAnswer[1];
+                thisQuestionData[4] = theGameData.virus.easy[randNum].AllAnswer[2];
+                thisQuestionData[5] = theGameData.virus.easy[randNum].AllAnswer[3];
+                return thisQuestionData;
+            }
+            else if(choice == "medium"){
+                thisQuestionData[0] = theGameData.virus.medium[randNum].Question;
+                thisQuestionData[1] = theGameData.virus.medium[randNum].CorrectAnswer;
+                thisQuestionData[2] = theGameData.virus.medium[randNum].AllAnswer[0];
+                thisQuestionData[3] = theGameData.virus.medium[randNum].AllAnswer[1];
+                thisQuestionData[4] = theGameData.virus.medium[randNum].AllAnswer[2];
+                thisQuestionData[5] = theGameData.virus.medium[randNum].AllAnswer[3];
+                return thisQuestionData;
+            }
+            else
+            {
+                thisQuestionData[0] = theGameData.virus.hard[randNum].Question;
+                thisQuestionData[1] = theGameData.virus.hard[randNum].CorrectAnswer;
+                thisQuestionData[2] = theGameData.virus.hard[randNum].AllAnswer[0];
+                thisQuestionData[3] = theGameData.virus.hard[randNum].AllAnswer[1];
+                thisQuestionData[4] = theGameData.virus.hard[randNum].AllAnswer[2];
+                thisQuestionData[5] = theGameData.virus.hard[randNum].AllAnswer[3];
+                return thisQuestionData;
+            }
+        }
+        else if(thechoice == "bacterias"){
+            if(choice == "easy"){
+                thisQuestionData[0] = theGameData.bacterias.easy[randNum].Question;
+                thisQuestionData[1] = theGameData.bacterias.easy[randNum].CorrectAnswer;
+                thisQuestionData[2] = theGameData.bacterias.easy[randNum].AllAnswer[0];
+                thisQuestionData[3] = theGameData.bacterias.easy[randNum].AllAnswer[1];
+                thisQuestionData[4] = theGameData.bacterias.easy[randNum].AllAnswer[2];
+                thisQuestionData[5] = theGameData.bacterias.easy[randNum].AllAnswer[3];
+                return thisQuestionData;
+            }
+            else if(choice == "medium"){
+                thisQuestionData[0] = theGameData.bacterias.medium[randNum].Question;
+                thisQuestionData[1] = theGameData.bacterias.medium[randNum].CorrectAnswer;
+                thisQuestionData[2] = theGameData.bacterias.medium[randNum].AllAnswer[0];
+                thisQuestionData[3] = theGameData.bacterias.medium[randNum].AllAnswer[1];
+                thisQuestionData[4] = theGameData.bacterias.medium[randNum].AllAnswer[2];
+                thisQuestionData[5] = theGameData.bacterias.medium[randNum].AllAnswer[3];
+                return thisQuestionData;
+            }
+            else
+            {
+                thisQuestionData[0] = theGameData.bacterias.hard[randNum].Question;
+                thisQuestionData[1] = theGameData.bacterias.hard[randNum].CorrectAnswer;
+                thisQuestionData[2] = theGameData.bacterias.hard[randNum].AllAnswer[0];
+                thisQuestionData[3] = theGameData.bacterias.hard[randNum].AllAnswer[1];
+                thisQuestionData[4] = theGameData.bacterias.hard[randNum].AllAnswer[2];
+                thisQuestionData[5] = theGameData.bacterias.hard[randNum].AllAnswer[3];
+                return thisQuestionData;
+            }
+        }
+        else if(thechoice == "fungus"){
+            if(choice == "easy"){
+                thisQuestionData[0] = theGameData.fungus.easy[randNum].Question;
+                thisQuestionData[1] = theGameData.fungus.easy[randNum].CorrectAnswer;
+                thisQuestionData[2] = theGameData.fungus.easy[randNum].AllAnswer[0];
+                thisQuestionData[3] = theGameData.fungus.easy[randNum].AllAnswer[1];
+                thisQuestionData[4] = theGameData.fungus.easy[randNum].AllAnswer[2];
+                thisQuestionData[5] = theGameData.fungus.easy[randNum].AllAnswer[3];
+                return thisQuestionData;
+            }
+            else if(choice == "medium"){
+                thisQuestionData[0] = theGameData.fungus.medium[randNum].Question;
+                thisQuestionData[1] = theGameData.fungus.medium[randNum].CorrectAnswer;
+                thisQuestionData[2] = theGameData.fungus.medium[randNum].AllAnswer[0];
+                thisQuestionData[3] = theGameData.fungus.medium[randNum].AllAnswer[1];
+                thisQuestionData[4] = theGameData.fungus.medium[randNum].AllAnswer[2];
+                thisQuestionData[5] = theGameData.fungus.medium[randNum].AllAnswer[3];
+                return thisQuestionData;
+            }
+            else
+            {
+                thisQuestionData[0] = theGameData.fungus.hard[randNum].Question;
+                thisQuestionData[1] = theGameData.fungus.hard[randNum].CorrectAnswer;
+                thisQuestionData[2] = theGameData.fungus.hard[randNum].AllAnswer[0];
+                thisQuestionData[3] = theGameData.fungus.hard[randNum].AllAnswer[1];
+                thisQuestionData[4] = theGameData.fungus.hard[randNum].AllAnswer[2];
+                thisQuestionData[5] = theGameData.fungus.hard[randNum].AllAnswer[3];
+                return thisQuestionData;
+            }
+        } else 
+        {return thisQuestionData;}
+    }
+
     void Start()
     {
-        theGameData = JsonUtility.FromJson<GameData>(textJSON.text);
-        Debug.Log(theGameData.virus.easy[0].AllAnswer[0]);
-        //Debug.Log(theGameData.virus.easy[0].AllAswer[3]);
-        //textChange.UpdateText(theGameData.virus.easy[0].Question);
+        
     }
 }
